df2c83573f5713c6e8c0e1eebff5828d31120487
Martin Drohmann <mdrohmann@gmail.com>
2015-05-11 04:15:40 -0700

    [generate_ssl_certificate]
    - add PEM file generation
    - remove clutter

--- scripts/generate_ssl_certificate.sh
+++ scripts/generate_ssl_certificate.sh
@@ -7,9 +7,13 @@ openssl genrsa -passout pass:x -des3 -out ${NAME}.pass.key 2048
 
 echo "Strip the password"
 openssl rsa -passin pass:x -in ${NAME}.pass.key -out ${NAME}.key
+rm ${NAME}.pass.key
 
 echo "Generate CSR"
 openssl req -nodes -new -key ${NAME}.key -out ${NAME}.csr
 
 echo "Generate self-signed certificate"
 openssl x509 -req -days 365 -in ${NAME}.csr -signkey ${NAME}.key -out ${NAME}.crt
+
+echo "Generate PEM file"
+cat ${NAME}.key ${NAME}.crt > ${NAME}.pem
